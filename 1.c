8af9f17ca4e481f29c211dcd50550577d394a485
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-07-08 23:00:13 +0200

    Update 1.c

--- 1.c
+++ 1.c
@@ -176,3 +176,134 @@ int	nbstr_cmp(const char *s1, const char *s2)
 	}
 	return ((unsigned char)s1[i] - (unsigned char)s2[j]);
 }
+
+/* fill_stack_values:
+*	Fills stack_a with the provided values.
+*	If the values are out of integer range, prints and error and exits the program.
+*/
+t_stack	*fill_stack_values(int ac, char **av)
+{
+	t_stack		*stack_a;
+	long int	nb;
+	int			i;
+
+	stack_a = NULL;
+	nb = 0;
+	i = 1;
+	while (i < ac)
+	{
+		nb = ft_atoi(av[i]);
+		if (nb > INT_MAX || nb < INT_MIN)
+			exit_error(&stack_a, NULL);
+		if (i == 1)
+			stack_a = stack_new((int)nb);
+		else
+			stack_add_bottom(&stack_a, stack_new((int)nb));
+		i++;
+	}
+	return (stack_a);
+}
+
+/* free_stack:
+*	Frees each element in a given stack and sets the stack pointer to NULL.
+*/
+void	free_stack(t_stack **stack)
+{
+	t_stack	*tmp;
+
+	if (!stack || !(*stack))
+		return ;
+	while (*stack)
+	{
+		tmp = (*stack)->next;
+		free(*stack);
+		*stack = tmp;
+	}
+	*stack = NULL;
+}
+
+/* exit_error:
+*   Writes "Error\n" to the standard output after freeing stack a and b.
+*	Exits with standard error code 1.
+*/
+void	exit_error(t_stack **stack_a, t_stack **stack_b)
+{
+	if (stack_a == NULL || *stack_a != NULL)
+		free_stack(stack_a);
+	if (stack_b == NULL || *stack_b != NULL)
+		free_stack(stack_b);
+	write(2, "Error\n", 6);
+	exit (1);
+}
+
+/* ft_atoi:
+*   Converts an alphanumeric string of characters into a long integer.
+*/
+long int	ft_atoi(const char *str)
+{
+	long int	nb;
+	int			isneg;
+	int			i;
+
+	nb = 0;
+	isneg = 1;
+	i = 0;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+	{
+		isneg *= -1;
+		i++;
+	}
+	while (is_digit(str[i]))
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	return (nb * isneg);
+}
+
+/* ft_putstr:
+*	Prints a given string of characters to the standard output.
+*/
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/* nb_abs:
+*	Returns the absolute value of a given number.
+*	The absolute value of a number is used to measure the distance of that
+*	number from 0, whether it is positive or negative (abs value of -6 is 6).
+*/
+int	nb_abs(int nb)
+{
+	if (nb < 0)
+		return (nb * -1);
+	return (nb);
+}
+
+/* add_stack_bottom:
+*	Adds an element to the bottom of a stack.
+*/
+void	stack_add_bottom(t_stack **stack, t_stack *new)
+{
+	t_stack	*tail;
+
+	if (!new)
+		return ;
+	if (!*stack)
+	{
+		*stack = new;
+		return ;
+	}
+	tail = get_stack_bottom(*stack);
+	tail->next = new;
+}
