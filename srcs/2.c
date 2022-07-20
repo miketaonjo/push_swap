16672b0c0f3438a3ce4aea2a56c8325d6f253950
miketaonjo <79211048+miketaonjo@users.noreply.github.com>
2022-07-20 23:20:37 +0200

    Update 2.c

--- srcs/2.c
+++ srcs/2.c
@@ -10,153 +10,34 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-typedef struct s_list
-{
-	int				value;
-	struct s_list	*next;
-}	t_list;
-
-typedef struct s_stack
-{
-	t_list	*stack_a;
-	t_list	*stack_b;
-}	t_stack;
-
-t_list	*new_elem(int n)
-{
-	t_list	*new;
-
-	new = malloc(sizeof(t_list));
-	if (!new)
-		return (NULL);
-	new->value = n;
-    new->prev = NULL;
-	new->next = NULL;
-	return (cell);
-}
-
-void	print_list(t_list *list)
-{
-	if (!list)
-		ft_printf("NULL\n");
-	while (list)
-	{
-		ft_printf("%d\n", lst->value);
-		lst = lst->next;
-	}
-}
-
-/*t_list	*add_at(t_list *L, int data, int pos)
-{
-	t_list	*prec;
-	t_list	*cur;
-	t_list	*cell;
-	int		i;
-
-	prec = L;
-	cur = L;
-	cell = create_cell(data);
-	if (is_empty_list(L))
-		return (cell);
-	if (pos == 0)
-	{
-		cell->next = L;
-		return (cell);
-	}
-	i = 0;
-	while (i++ < pos)
-	{
-		prec = cur;
-		cur = cur->next;
-	}
-	prec->next = cell;
-	cell->next = cur;
-	return (L);
-}*/
-
-t_list	*add_elem(t_list *lst, int value, int position)
-{
-	t_list	*new;
-	int		i;
-    
-	new = new_elem(value);
-	if (lst == NULL)
-		return (new);
-	if (position == 0)
-	{
-		new->next = lst;
-		return (new);
-	}
-	i = 0;
-	while (i < position)
-	{
-		new->prev = lst;
-		lst = lst->next;
-        i++;
-	}
-	lst->next = new;
-	new->next = lst;
-	return (lst);
-}
-
-t_list	*parse_list_int(char **argv, t_list	*a)
-{
-	int		i;
-	int		value;
-
-	i = 0;
-	while (argv[i])
-	{
-		if (i == 0)
-			i++;
-		value = ft_atoli(argv[i]);
-		a = add_elem(a, value, i);
-		i++;
-	}
-	print_list(a);
-	return (a);
-}
-
-t_list	*parse_list_str(char *str, t_list *a)
-{
-	int		value;
-	char	**tab;
-	int		i;
-
-	tab = ft_split(str, ' ');
-	i = 0;
-	while (tab[i])
-	{
-		value = ft_atoli(tab[i]);
-		a = add_elem(a, value, i);
-        i++;
-	}
-	print_list(a);
-	return (a);
-}
-
 int	main(int argc, char **argv)
 {
-	t_list	*a;
-	t_list	*b;
+	t_list	*list_a;
+	t_list	*list_b;
 
-	a = NULL;
-	b = NULL;
-
+	list_a = empty_list();
+	list_b = empty_list();
+	if (argc == 1)
+		ft_error();
 	if (argc == 2)
-		a = parse_list_str(argv[1], a);
+		list_a = parse_list_str(argv[1], list_a);
 	if (argc > 2)
-		a = parse_list_int(argv, a);
-	check_algo(&a, &b);
+		list_a = parse_list_int(argv, list_a);
+	/*printf("--->%d\n", list_len(list_a));
+	push(&list_a, &list_b);
+	swap(&list_a);
+	push(&list_a, &list_b);
+	rotate(&list_a);
+	reverse_rotate(&list_b);*/
+	check_algo(&list_a, &list_b);
 	printf("list_a:\n");
-	print_list(a);
+	print_list(list_a);
 	printf("list_b:\n");
-	print_list(b);
+	print_list(list_b);
 	return (0);
 }
 
 //moves
-
 
 void	rotate(t_list **lst)
 {
@@ -179,4 +60,146 @@ void	reverse_rotate(t_list **list)
 	temp = get_elem(*list, list_len(*list) - 1);
 	*list = free_at(*list, list_len(*list) - 1);
 	*list = add_at(*list, temp, 0);
+}
+
+//algo
+int	check_algo(t_list **list_a, t_list **list_b)
+{
+	if (list_len(*list_a) == 2 && (*list_a)->data > (*list_a)->next->data)
+		swap(list_a);
+	if (list_len(*list_a) == 3)
+		small_algo(list_a);
+	if (list_len(*list_a) == 5)
+		med_algo(list_a, list_b);
+	return (0);
+}
+
+void	small_algo(t_list **list)
+{
+	if ((*list)->next->data < (*list)->data
+		&& (*list)->data < (*list)->next->next->data)
+		swap(list);
+	if ((*list)->next->next->data < (*list)->next->data
+		&& (*list)->next->data < (*list)->data)
+	{
+		rotate(list);
+		swap(list);
+	}
+	if ((*list)->next->data < (*list)->next->next->data
+		&& (*list)->next->next->data < (*list)->data)
+		rotate(list);
+	if ((*list)->data < (*list)->next->next->data
+		&& (*list)->next->next->data < (*list)->next->data)
+	{
+		swap(list);
+		rotate(list);
+	}
+	if ((*list)->next->next->data < (*list)->data
+		&& (*list)->data < (*list)->next->data)
+		reverse_rotate(list);
+}
+
+void	check_elem(t_list **list_a, t_list **list_b)
+{
+	int	temp;
+	int	i;
+	
+	if ((*list_a)->data > (*list_a)->next->data)
+	{
+		i = 1;
+		temp = (*list_a)->data;
+		while (temp > get_elem(*list_a, i) || i == list_len(*list_a))
+		{
+			i++;
+			if (i == list_len(*list_a))
+				break ;
+		}
+		*list_a = free_at(*list_a, 0);
+		*list_a = add_at(*list_a, temp, i - 1);
+	}
+}
+
+void	med_algo(t_list **list_a, t_list **list_b)
+{
+	push(list_a, list_b);
+	push(list_a, list_b);
+	check_algo(list_a, list_b);
+	push(list_b, list_a);
+	check_elem(list_a, list_b);
+	push(list_b, list_a);
+	check_elem(list_a, list_b);
+}
+
+//utils
+
+int	list_len(t_list *L)
+{
+	int	i;
+
+	i = 0;
+	while (L)
+	{
+		i++;
+		L = L->next;
+	}
+	return (i);
+}
+
+t_list	*free_at(t_list *L, int pos)
+{
+	t_list	*prec;
+	t_list	*cur;
+	int		i;
+
+	prec = L;
+	cur = L;
+	if (is_empty_list(L))
+		return (NULL);
+	if (pos == 0)
+	{
+		L = L->next;
+		free(cur);
+		return (L);
+	}
+	i = 0;
+	while (i++ < pos)
+	{
+		prec = cur;
+		cur = cur->next;
+	}
+	prec->next = cur->next;
+	free(cur);
+	return (L);
+}
+
+t_list	*free_list(t_list *L)
+{
+	t_list	*tmp;
+
+	tmp = NULL;
+	while (L)
+	{
+		tmp = L->next;
+		free(L);
+		L = tmp;
+	}
+	return (L);
+}
+
+int		get_elem(t_list *L, int pos)
+{
+	int	i;
+
+	i = 0;
+	if (pos > list_len(L))
+	{
+		printf("print_elem : position given higher than length of list...\n");
+		exit (0);
+	}
+	while (i < pos)
+	{
+		i++;
+		L = L->next;
+	}
+	return (L->data);
 }
